bef8b6ded2712f0c4086486b9c98c0d2fe5d04a7
Rafał Andrzejewski <>
2023-05-07 22:25:01 +0200

    fix

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,9 +1,12 @@
 import Card from './Card';
 import FormField from './FormField';
 import Loader from './Loader';
+import FormSlider from './FormSlider';
 
 export {
   Card,
   FormField,
   Loader,
+  FormSlider,
+  
 };
