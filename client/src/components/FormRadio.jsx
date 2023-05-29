c2fa26339544e99cf7618009205511d8ed92614d
Puc3k <kacperplocienniczak20@gmail.com>
2023-05-29 23:57:23 +0200

    beauty: code clean up

--- client/src/components/FormRadio.jsx
+++ client/src/components/FormRadio.jsx
@@ -37,31 +37,4 @@ const FormRadio = ({labelName, name, options, values, handleChange, key = null})
     );
 }
 
-// const FormRadioTest2 = props => {
-//     const [gender, setGender] = React.useState();
-//     const [role, setRole] = React.useState();
-//     const handleSubmit = e => {
-//         e.preventDefault();
-//         const data = {gender, role};
-//         const json = JSON.stringify(data, null, 4);
-//         console.clear();
-//         console.log(json);
-//     };
-//     return (
-//         <form onSubmit={handleSubmit}>
-//             <div>
-//                 <label>Gender:</label>
-//                 <input type="radio" label="Male" value="male" checked={gender} setter={setGender}/>
-//                 <input label="Female" value="female" checked={gender} setter={setGender}/>
-//             </div>
-//             <div>
-//                 <label>Role:</label>
-//                 <input label="Admin" value="admin" checked={role} setter={setRole}/>
-//                 <input label="User" value="user" checked={role} setter={setRole}/>
-//             </div>
-//             <button type="submit">Submit</button>
-//         </form>
-//     );
-// };
-
 export default FormRadio;
